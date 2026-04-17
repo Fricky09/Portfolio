e2e03620605085685693f49d31add8f2a37a7d42
Ismael Jauro <ismaeljauro0@gmail.com>
2026-04-17 11:05:22 +0100

    Final round

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+const menuIcon = document.getElementById("menu-icon");
+const navLinks = document.querySelector(".nav-links");
 
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-}
+menuIcon.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+
+    // icon morph
+    menuIcon.classList.toggle("bx-menu");
+    menuIcon.classList.toggle("bx-x");
+});
 
 document.getElementById("contact-form").addEventListener("submit", function (e) {
     e.preventDefault();
